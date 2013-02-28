9c5fca3131058c6775a60705e8ea7478a025c0f9
Adam Hyland <protonk@gmail.com>
2013-02-28 13:25:39 -0500

    update ONS. Add NISRA.

---
+++ src/gather/NISRAbabyget.R
@@ -0,0 +1,3 @@
+# Ireland urls
+
+# http://www.nisra.gov.uk/archive/demography/publications/babynames/Full_Name_List_9711.xls
--- src/gather/ONSbabyget.R
+++ src/gather/ONSbabyget.R
@@ -2,9 +2,38 @@ library(XML)
 
 # UK
 
-xpathSApply("http://www.ons.gov.uk/ons/rel/vsob1/baby-names--england-and-wales/index.html", "//div[@class = 'previous-releases-results']//a", xmlAttrs)
+ons.base.url <- "http://www.ons.gov.uk"
 
-htmlParse("http://www.ons.gov.uk/ons/rel/vsob1/baby-names--england-and-wales/2003/index.html")
+# index page for data 
+paste0(ons.base.url, "/ons/rel/vsob1/baby-names--england-and-wales/index.html")
+
+index.doc <- htmlParse(file.path(ons.base.url, "rel/vsob1/baby-names--england-and-wales/index.html"))
+
+
+# somewhat fragile path to individual data pages
+xpathSApply(index.doc, "//div[@class = 'previous-releases-results']//a", xmlAttrs)
+
+year.pages <- paste0(ons.base.url, xpathSApply(index.doc, "//div[@class = 'previous-releases-results']//a", xmlAttrs))
+
+# drop summary page for now
+year.pages <- year.pages[!grepl("1904-1994", year.pages)]
+
+tableGet <- function(url) {
+  tables <- xpathSApply(htmlParse(url), "//div[@class = 'srp-pubs-list']//a", xmlAttrs)
+  tables <- tables[grepl("reference", tables)]
+  excel.out <- xpathSApply(htmlParse(paste0(ons.base.url, tables)),
+                           "//div[@class = 'download-options']//a[@class  = 'xls']",
+                           xmlAttrs)
+  return(excel.out)
+}
+
+year.tables <- sapply(year.pages, tableGet)
+
+# names(year.tables) <- basename(sub("([0-9]{4})/.*", "\\1", names(year.tables)))
+
+year.tables <- paste0(ons.base.url, year.tables)
+
+
 
 ## Scotland
 
