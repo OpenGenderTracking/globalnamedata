f75cab016ce26c54036fc91dea5a92f9044f173c
Adam Hyland <protonk@gmail.com>
2013-03-08 11:40:32 -0500

    convert ONS and SSA scripts to enclosed functions. Move/remove imports. rm some cleanup lines.

--- src/gather/ONSbabyget.R
+++ src/gather/ONSbabyget.R
@@ -1,110 +1,121 @@
-# scraping ONS index pages
-library(XML)
-
-# necessary because ONS keeps records as .xls (multi-sheet)
-# reading XLS files with gdata requires a perl installation
-library(gdata)
-# RCurl necessary because read.xls will fail gracelessly 
-# on malformed downloads, so we use writeBin
-library(RCurl)
-# if needed, the path to perl can be set as an argument here
-installXLSXsupport()
+#####
+# Read names from the UK's Office of National Statistics (ONS)
+#
+# Names are included in Excel spreadsheets broken up by gender
+# and year. Links to those spreadsheets are gathered from 
+# the data navigator provided by ONS
+#####
 
 
-# somewhat fragile path to individual data pages
 
-ons.base.url <- "http://www.ons.gov.uk"
+readONSNames <- function() {
+  # scraping ONS index pages
+  library(XML)
 
-indexGet <- function() {
-  # index page for data 
-  
-  ons.index <- "rel/vsob1/baby-names--england-and-wales/index.html"
-  index.doc <- htmlParse(file.path(ons.base.url, ons.index))
-  year.pages <- xpathSApply(index.doc, 
-                          "//div[@class = 'previous-releases-results']//a", 
+  # necessary because ONS keeps records as .xls (multi-sheet)
+  # reading XLS files with gdata requires a perl installation
+  library(gdata)
+  # RCurl necessary because read.xls will fail gracelessly 
+  # on malformed downloads, so we use writeBin
+  library(RCurl)
+  # if needed, the path to perl can be set as an argument here
+  installXLSXsupport()
+  ons.base.url <- "http://www.ons.gov.uk"
+  # somewhat fragile path to individual data pages
+  indexGet <- function() {
+    # index page for data 
+    
+    ons.index <- "rel/vsob1/baby-names--england-and-wales/index.html"
+    index.doc <- htmlParse(file.path(ons.base.url, ons.index))
+    year.pages <- xpathSApply(index.doc, 
+                            "//div[@class = 'previous-releases-results']//a", 
+                            xmlAttrs)
+
+    year.pages <- paste(ons.base.url,
+                        year.pages,
+                        sep="")
+    # drop summary page for now
+    year.pages <- year.pages[!grepl("1904-1994", year.pages)]
+    return(year.pages)
+  }
+
+
+  year.pages <- indexGet()
+
+
+  tableGet <- function(url) {
+    tables <- xpathSApply(htmlParse(url), 
+                          "//div[@class = 'srp-pubs-list']//a", 
                           xmlAttrs)
-
-  year.pages <- paste(ons.base.url,
-                      year.pages,
-                      sep="")
-  # drop summary page for now
-  year.pages <- year.pages[!grepl("1904-1994", year.pages)]
-  return(year.pages)
-}
-
-
-year.pages <- indexGet()
-
-
-tableGet <- function(url) {
-  tables <- xpathSApply(htmlParse(url), 
-                        "//div[@class = 'srp-pubs-list']//a", 
-                        xmlAttrs)
-  tables <- tables[grepl("reference", tables)]
-  excel.out <- xpathSApply(htmlParse(paste(ons.base.url, tables, sep="")),
-                           "//div[@class = 'download-options']//a[@class  = 'xls']",
-                           xmlAttrs)
-  # the class and the href are both attributes
-  return(excel.out[1, ])
-}
-
-year.tables <- sapply(year.pages, tableGet)
-
-year.tables <- paste0(ons.base.url, year.tables)
-
-# drop columns which are auto-named
-
-wrapXLS <- function(url) {
-  downloadXLS <- function(url) {
-    # Download files as binary and push to a temp directory
-    # because read.xls has problems w/ web downloads
-    temp.file <- tempfile(pattern = "ONS", fileext = ".xls")
-    f <- getBinaryURL(url)
-    writeBin(f, temp.file)
-    return(temp.file)
+    tables <- tables[grepl("reference", tables)]
+    excel.out <- xpathSApply(htmlParse(paste(ons.base.url, tables, sep="")),
+                             "//div[@class = 'download-options']//a[@class  = 'xls']",
+                             xmlAttrs)
+    # the class and the href are both attributes
+    return(excel.out[1, ])
   }
 
-  temp.file <- downloadXLS(url)
+  year.tables <- sapply(year.pages, tableGet)
 
-  # find which sheet we need to look at as well as
-  # which gender are we looking at.
-  sheet.names <- sheetNames(temp.file)
-  sheet.loc <- grepl("(Boy|Girl)s names", sheet.names)
-  if (any(sheet.loc)) {
-    # only will happen if ONS changes structure of xls files
-    if (sum(sheet.loc) > 1) {
-      stop("too many sheets found")
+  year.tables <- paste0(ons.base.url, year.tables)
+
+  # drop columns which are auto-named
+
+  wrapXLS <- function(url) {
+    downloadXLS <- function(url) {
+      # Download files as binary and push to a temp directory
+      # because read.xls has problems w/ web downloads
+      temp.file <- tempfile(pattern = "ONS", fileext = ".xls")
+      f <- getBinaryURL(url)
+      writeBin(f, temp.file)
+      return(temp.file)
     }
-    sheet.number <- which(sheet.loc)
 
-    xls.df <- read.xls(temp.file, sheet = sheet.number, method = "csv",
-                       skip = 2, stringsAsFactors = FALSE)
-    xls.df <- xls.df[, names(xls.df)[!grepl("X(\\.?[0-9]*)?", names(xls.df))]]
-    xls.df[, "Sex"] <- ifelse(grepl("Boy", sheet.names[sheet.loc]), "M", "F")
-    xls.df[, "Year"] <- basename(sub("([0-9]{4})/[^/]*$", "\\1", url))
-  } else {
-    stop("no full sheet found")
+    temp.file <- downloadXLS(url)
+
+    # find which sheet we need to look at as well as
+    # which gender are we looking at.
+    sheet.names <- sheetNames(temp.file)
+    sheet.loc <- grepl("(Boy|Girl)s names", sheet.names)
+    if (any(sheet.loc)) {
+      # only will happen if ONS changes structure of xls files
+      if (sum(sheet.loc) > 1) {
+        stop("too many sheets found")
+      }
+      sheet.number <- which(sheet.loc)
+
+      xls.df <- read.xls(temp.file, sheet = sheet.number, method = "csv",
+                         skip = 2, stringsAsFactors = FALSE)
+      xls.df <- xls.df[, names(xls.df)[!grepl("X(\\.?[0-9]*)?", names(xls.df))]]
+      xls.df[, "Sex"] <- ifelse(grepl("Boy", sheet.names[sheet.loc]), "M", "F")
+      xls.df[, "Year"] <- basename(sub("([0-9]{4})/[^/]*$", "\\1", url))
+    } else {
+      stop("no full sheet found")
+    }
+
+    unlink(temp.file)
+    closeAllConnections()
+
+    # cleanup df
+    # ONS reports numbers as strings with commas, etc.
+    xls.df[, "Count"] <- as.numeric(gsub(",|\\.|;", "", xls.df[, "Count"]))
+    xls.df <- xls.df[, c("Name", "Count", "Sex", "Year")]
+    xls.df <- xls.df[grepl("^[^A-Z]*$", xls.df[, "Name"]), ]
+    xls.df <- xls.df[complete.cases(xls.df[, "Count"]), ]
+
+    return(xls.df)
   }
 
-  unlink(temp.file)
-  closeAllConnections()
+  alluk.df <- do.call(rbind, lapply(year.tables, wrapXLS))
 
-  # cleanup df
-  # ONS reports numbers as strings with commas, etc.
-  xls.df[, "Count"] <- as.numeric(gsub(",|\\.|;", "", xls.df[, "Count"]))
-  xls.df <- xls.df[, c("Name", "Count", "Sex", "Year")]
-  xls.df <- xls.df[complete.cases(xls.df[, "Count"]), ]
+  ## should use matchSexes() from SSAbabyget
+  ## really, those should be loaded first.
 
-  return(xls.df)
+  alluk.df <- ddply(alluk.df, "Year", function(x) matchSexes(x))
+  return(alluk.df)
 }
 
-alluk.list <- lapply(year.tables, wrapXLS)
+# output
+# alluk.df <- readONSNames()
 
-
-# cleanup df
-
-## should use matchsexes from SSAbabyget
-## really, those should be loaded first.
-
-# alluk.df <- ddply(alluk.df, "Year", function(x) matchSexes(x))
 
--- src/gather/SSAbabyget.R
+++ src/gather/SSAbabyget.R
@@ -1,95 +1,55 @@
 #####
-#
 # Download Baby Names from SSA and flatten into single data file
-#
+# 
+# The SSA provides all name data in a single zip file which contains
+# .csv files for each year. Both genders are included in each year
+# file
 #####
 
-# libraries required for the last step. 
-library(plyr)
-library(reshape2)
 
 ## Setup temp files
 # Not strictly necessary, but we don't need to retain the zip
+readSSANames <- function() {
+  names.tmpdir <- tempdir()
+  temp <- tempfile()
+  output.file <- file.path(getwd(), "data/SSA.csv")
 
-names.tmpdir <- tempdir()
-temp <- tempfile()
-output.file <- file.path(getwd(), "data/SSA.csv")
+  # download and unzip
+  # See http://www.ssa.gov/oact/babynames/limits.html for info 
+  download.file('http://www.ssa.gov/oact/babynames/names.zip', temp)
+  unzip(temp, exdir = names.tmpdir)
 
-# download and unzip
-# See http://www.ssa.gov/oact/babynames/limits.html for info 
-download.file('http://www.ssa.gov/oact/babynames/names.zip', temp)
-unzip(temp, exdir = names.tmpdir)
-
-# get vector of path names
-# sanity check list if working in an RStudio project
-yr.name.files <- list.files(path = names.tmpdir, 
-                            pattern = "yob[0-9]{4}\\.txt", 
-                            full.names = TRUE)
+  # get vector of path names
+  # sanity check list if working in an RStudio project
+  yr.name.files <- list.files(path = names.tmpdir, 
+                              pattern = "yob[0-9]{4}\\.txt", 
+                              full.names = TRUE)
 
 
-# Read a csv from a file, adding the year from the file to 
-# a new column
-readWrap <- function(filepath) {
-  
-  yr.out <- read.csv(filepath, 
-                     col.names = c("Name", "Sex", "Count"), 
-                     header = FALSE, as.is = TRUE)
-  yr.out[, "Year"] <- gsub("yob([0-9]{4})\\.txt", "\\1", basename(filepath))
-  return(yr.out)
+  # Read a csv from a file, adding the year from the file to 
+  # a new column
+  readWrap <- function(filepath) {
+    
+    yr.out <- read.csv(filepath, 
+                       col.names = c("Name", "Sex", "Count"), 
+                       header = FALSE, as.is = TRUE)
+    yr.out[, "Year"] <- gsub("yob([0-9]{4})\\.txt", "\\1", basename(filepath))
+    return(yr.out)
+  }
+
+  # Combine all years into a single dataframe
+
+  us.names.df <- do.call(rbind, lapply(yr.name.files, readWrap))
+
+
+  # this will take a while. You're looping over 100+ years 
+  # comprising ~2 million rows
+  us.names.df <- ddply(us.names.df, "Year", function(x) matchSexes(x))
+  unlink(c(names.tmpdir, temp))
+  return(us.names.df)
 }
 
-# Combine all years into a single dataframe
+# read and output to data frame
+# us.names.df <- readSSANames()
 
-us.names.df <- do.call(rbind, lapply(yr.name.files, readWrap))
-
-# Condense names to single row
-
-matchSexes <- function(x) {
-  # melt and cast are two broad data handling patterns
-  # think of them as the two steps in constructing a
-  # pivot table:
-  
-  # x is of the following form:
-  #        Name Sex Count Year
-  # 1      Mary   F  7065 1880
-  # 2      Anna   F  2604 1880
-  # 3      Emma   F  2003 1880
-  # 4 Elizabeth   F  1939 1880
-  # 5    Minnie   F  1746 1880
-  # 6  Margaret   F  1578 1880
-  
-  x.out <- dcast(x[, c("Name", "Sex", "Count")], Name ~ Sex, sum, value.var = "Count")
-  ## x.out structure
-  #     Name  F   M
-  # 1  Aaron  0 102
-  # 2     Ab  0   5
-  # 3  Abbie 71   0
-  # 4 Abbott  0   5
-  # 5   Abby  6   0
-  # 6    Abe  0  50
-  
-  # Add a year column. Year is the same for all rows since
-  # names are grouped per file per year.
-  # Faster/safer than unique(x[, "Year"])
-  # no easy way to extract from the passed argument
-  x.out[, "Year"] <- x[, "Year"][1]
-  
-  return(x.out)
-}
-
-# this will take a while. You're looping over 100+ years 
-# comprising ~2 million rows
-us.names.df <- ddply(us.names.df, "Year", function(x) matchSexes(x))
-
-
-## Cleanup from import
-# not required but still
-unlink(c(names.tmpdir, temp))
-rm(temp, yr.name.files, names.tmpdir, readWrap)
-
-
-# persist file to disk
-
-write.table(us.names.df, output.file, 
-            quote=FALSE, sep=",", row.names = FALSE, col.names = TRUE)
 
